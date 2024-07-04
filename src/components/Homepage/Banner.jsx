c3e29c6833d90cf562c61f89729684dd0ecb6dfd
tanvir244 <tr30365s25@gmail.com>
2024-07-04 16:24:53 +0600

    About Section complete with responsive design

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -6,11 +6,11 @@ const Banner = () => {
     return (
         <div className='flex flex-col md:flex-row justify-between h-[70vh] items-center gap-6'>
             <div className='w-full md:w-1/2'>
-                <h1 className='text-3xl lg:text-5xl font-bold text-white mb-4'>Tanvir Rahman Saim</h1>
-                <p className='text-[#858383] text-md font-normal mb-7'>As a passionate learner, I have dedicated myself to acquiring the skill set of web application development. Throughout this extensive journey, my primary focus has been on frontend development. Along the way, I have successfully completed numerous full-stack projects with a focus on frontend development.</p>
+                <h1 className='text-3xl lg:text-5xl font-bold text-white mb-4'>Tanvir Rahman</h1>
+                <p className='text-[#858383] text-md font-normal mb-7'>Currently, I am working with JavaScript technologies such as Next.js, React.js, Node.js, and Express.js to create scalable full-stack applications. I am also focusing significantly on vanilla JavaScript to clarify my concepts and deepen my understanding of it.</p>
 
                 <ul className='flex gap-6 text-white'>
-                    <Link href='https://www.linkedin.com/in/hellotanvir4/' className='flex items-center gap-2 text-[#faf0ca] font-semibold'><div className='w-3 h-3 bg-white bg-[#faf0ca] rounded-full'></div>LinkedIn</Link>
+                    <Link href='https://www.linkedin.com/in/hellotanvir4/' className='flex items-center gap-2 text-[#faf0ca] font-semibold'><div className='w-3 h-3 bg-[#faf0ca] rounded-full'></div>LinkedIn</Link>
                     <Link href='https://github.com/tanvir244' className='flex items-center gap-2 text-[#fb6f92] font-semibold'><div className='w-3 h-3 bg-[#fb6f92] rounded-full'></div>Github</Link>
                     <Link href='https://www.facebook.com/tanvirrahman.saim.7' className='flex items-center gap-2 text-[#4ecdc4] font-semibold'><div className='w-3 h-3 bg-[#4ecdc4] rounded-full'></div>Facebook</Link>
                 </ul>
