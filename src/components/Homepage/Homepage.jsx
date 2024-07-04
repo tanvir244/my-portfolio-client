055871ab5183a25b906e2057f3c220b68ff28569
tanvir244 <tr30365s25@gmail.com>
2024-07-04 06:07:23 +0600

    Homepage completed with responsive navbar

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Image from 'next/image';
 
 const Homepage = () => {
     return (
-        <div>
+        <div className='w-[90%] lg:max-w-6xl mx-auto'>
             <Navbar />
+            <div className='flex flex-col md:flex-row justify-between h-[70vh] items-center gap-6'>
+                <div className='w-full md:w-1/2 space-y-4'>
+                    <h1 className='text-3xl lg:text-5xl font-bold text-white'>Tanvir Rahman Saim</h1>
+                    <p className='text-[#858383] text-md font-normal'>As a passionate learner, I have dedicated myself to acquiring the skill set of web application development. Throughout this extensive journey, my primary focus has been on frontend development. Along the way, I have successfully completed numerous full-stack projects with a focus on frontend development.</p>
+                </div>
+                <div className='w-full md:w-1/2 flex justify-end'>
+                    <Image className='rounded-2xl' alt='author' src="https://i.ibb.co/6mMsMt0/Untitled-design-5.png" width={400} height={400}/>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Homepage/Navbar.jsx
+++ src/components/Homepage/Navbar.jsx
@@ -28,13 +28,36 @@ const Navbar = () => {
     ]
 
     return (
-        <div className="navbar max-w-6xl mx-auto text-white py-6">
-            <div className="flex-1">
+        <div className="navbar text-white py-6">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-primary rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {navItems.map(item => (
+                            <li key={item.path}><Link href={item.path}>{item.title}</Link></li>
+                        ))}
+                    </ul>
+                </div>
                 <Link href={'/'}>
-                    <Image alt='logo' src={'/logo3.png'} width={60} height={40}/>
+                    <Image alt='logo' src={'/logo3.png'} width={60} height={40} />
                 </Link>
             </div>
-            <div className="flex-none">
+            <div className="navbar-end hidden md:flex w-[78%]">
                 <ul className="menu menu-horizontal px-1">
                     {navItems.map(item => (
                         <li key={item.path}><Link href={item.path}>{item.title}</Link></li>
